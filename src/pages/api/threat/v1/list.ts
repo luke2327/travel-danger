da49f76f4b2c665c354f5157f6d3387a5bb5db3b
luke2327 <qwepoi8526@gmail.com>
2023-08-25 17:39:25 +0900

    feat: 알수없는 에러 10

--- src/pages/api/threat/v1/list.ts
+++ src/pages/api/threat/v1/list.ts
@@ -15,7 +15,7 @@ export default async function handler(
 ) {
   await openGate(req, res);
 
-  let body = req.query as {
+  let body = req.body as {
     keyword: string;
     locale: SupportedLanguage;
   };
@@ -28,3 +28,69 @@ export default async function handler(
 
   res.status(200).json({ result });
 }
+
+// import type { AxiosRequestConfig } from "axios";
+// import axios from "axios";
+// import type { NextApiRequest, NextApiResponse } from "next";
+//
+// import { openGate } from "@/libs/common";
+// import { getThreatList } from "@/libs/threat.service";
+// import type { SupportedLanguage } from "@/models/language";
+//
+// type Data = {
+//   result: any;
+//   result2: any;
+//   body: any;
+//   msg: any;
+// };
+//
+// const config = {
+//   method: "get",
+//   maxBodyLength: Infinity,
+// } as AxiosRequestConfig;
+//
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data>
+// ) {
+//   await openGate(req, res);
+//
+//   let body = req.body as {
+//     keyword: string;
+//     locale: SupportedLanguage;
+//   };
+//
+//   if (typeof body === "string" && body !== "") {
+//     body = JSON.parse(body);
+//   }
+//
+//   config.url = "https://api.terrorless.01ab.net/trpc/threat.list";
+//
+//   const response = await axios
+//     .request(config)
+//     .then((response) => response.data)
+//     .catch((error) => {
+//       console.log(error);
+//     });
+//
+//   let result2: any;
+//   let msg: any;
+//   try {
+//     result2 = await getThreatList(body);
+//   } catch (e) {
+//     msg = (e as any).toString();
+//   }
+//
+//   const result = response.result.data.json.threats.map((x: any) => {
+//     const { objectType, id, ...r } = x;
+//
+//     return r;
+//   });
+//
+//   res.status(200).json({
+//     result,
+//     body,
+//     result2,
+//     msg,
+//   });
+// }
