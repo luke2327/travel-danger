9d6b71d2aa256ac76e14ca8f5fccae105831c99b
luke2327 <qwepoi8526@gmail.com>
2023-09-12 21:38:12 +0900

    add: i18n shelter map

--- src/components/KakaoMapShelter.tsx
+++ src/components/KakaoMapShelter.tsx
@@ -6,14 +6,19 @@ import { makeMutable } from "@/libs/common";
 
 import type { ShelterList } from "@/models/safeMapList";
 import shelterListModel from "@/models/safeMapList";
+import type { SupportedLanguage } from "@/models/language";
 import KakaoMapMarkerShelter from "./KakaoMapMarkerShelter";
 
 const KakaoMapShelter = ({
   lat,
-  lng
+  lng,
+  locale,
+  isPhone
 }: {
   lat: string;
   lng: string;
+  locale: SupportedLanguage;
+  isPhone: boolean;
 }) => {
   const mapRef = useRef<any>(null);
   const [coordinate, setCoordinate] = useState<Record<"lat" | "lng", number>>({
@@ -21,7 +26,7 @@ const KakaoMapShelter = ({
     lng: 127.0071,
   });
   const [shelterList, setShelterList] = useState<ShelterList>(
-    shelterListModel.filter((x) => x.lcinfoLa && x.lcinfoLo)
+    shelterListModel.filter((x: ShelterList[0]) => x.lcinfoLa && x.lcinfoLo)
   );
   const [isOpen, setIsOpen] = useState<Record<number, boolean>>({});
 
@@ -30,7 +35,7 @@ const KakaoMapShelter = ({
   };
 
   useEffect(() => {
-    locationChange("서울");
+    setLocalAreaMap();
 
     if (lat && lng) {
       setCoordinate({
@@ -39,6 +44,27 @@ const KakaoMapShelter = ({
       });
     }
   }, []);
+
+  const setLocalAreaMap = () => {
+    let filter = (x: ShelterList[0]) => x.adres?.includes("서울");
+    if (isPhone && lat && lng) {
+      filter = (x: ShelterList[0]) => (
+        x.lcinfoLa &&
+        x.lcinfoLo &&
+        Math.abs(x.lcinfoLa - Number(lat)) < 0.02 &&
+        Math.abs(x.lcinfoLo - Number(lng)) < 0.02
+      ) as boolean;
+    } else if (!isPhone && lat && lng) {
+      filter = (x: ShelterList[0]) => (
+        x.lcinfoLa &&
+        x.lcinfoLo &&
+        Math.abs(x.lcinfoLa - Number(lat)) < 0.03 &&
+        Math.abs(x.lcinfoLo - Number(lng)) < 0.03
+      ) as boolean;
+    }
+
+    setShelterList(shelterListModel.filter(filter));
+  };
 
   const markerSelect = (index: number, lat: number, lng: number) => {
     const mutable = makeMutable(isOpen);
@@ -111,7 +137,7 @@ const KakaoMapShelter = ({
               </>
             ))}
         </Map>
-        <LocationSelector handleChange={locationChange as any} />
+        <LocationSelector handleChange={locationChange as any} locale={locale} />
       </div>
     </>
   );
@@ -119,7 +145,95 @@ const KakaoMapShelter = ({
 
 export default KakaoMapShelter;
 
-function LocationSelector({ handleChange }: { handleChange: () => void }) {
+const localLanguageMenu: Record<SupportedLanguage, { [key in string]: { label: string; value: string } }> = {
+  ko: {
+    metropolitan: { label: '수도권', value: '수도권' },
+    seoul: { label: '서울', value: '서울' },
+    kyeongkido: { label: '경기도', value: '경기도' },
+    kangwon: { label: '강원', value: '강원' },
+    kangwondo: { label: '강원도', value: '강원도' },
+    chungcheongdo: { label: '충청도', value: '충청도' },
+    chungcheongbukdo: { label: '충청북도', value: '충청북도' },
+    chungcheongnamdo: { label: '충청남도', value: '충청남도' },
+    kyungsangdo: { label: '경상도', value: '경상도' },
+    kyungsangbukdo: { label: '경상북도', value: '경상북도' },
+    kyungsangnamdo: { label: '경상남도', value: '경상남도' },
+    jeonlado: { label: '전라도', value: '전라도' },
+    jeonlabukdo: { label: '전라북도', value: '전라북도' },
+    jeonlanamdo: { label: '전라남도', value: '전라남도' },
+    jejudo: { label: '제주도', value: '제주도' },
+  },
+  en: {
+    metropolitan: { label: 'metropolitan area', value: '수도권' },
+    seoul: { label: 'Seoul', value: '서울' },
+    kyeongkido: { label: 'Gyeonggi-do', value: '경기도' },
+    kangwon: { label: 'Gangwon', value: '강원' },
+    kangwondo: { label: 'Gangwon-do', value: '강원도' },
+    chungcheongdo: { label: 'Chungcheong-do', value: '충청도' },
+    chungcheongbukdo: { label: 'Chungcheongbuk-do', value: '충청북도' },
+    chungcheongnamdo: { label: 'Chungcheongnam-do', value: '충청남도' },
+    kyungsangdo: { label: 'Gyeongsangdo', value: '경상도' },
+    kyungsangbukdo: { label: 'Gyeongsangbuk-do', value: '경상북도' },
+    kyungsangnamdo: { label: 'Gyeongsangnam-do', value: '경상남도' },
+    jeonlado: { label: 'Jeollado', value: '전라도' },
+    jeonlabukdo: { label: 'Jeollabuk-do', value: '전라북도' },
+    jeonlanamdo: { label: 'Jeollanam-do', value: '전라남도' },
+    jejudo: { label: 'Jeju Island', value: '제주도' },
+  },
+  ja: {
+    metropolitan: { label: '大都市圏', value: '수도권' },
+    seoul: { label: 'ソウル', value: '서울' },
+    kyeongkido: { label: '京畿道', value: '경기도' },
+    kangwon: { label: '江原', value: '강원' },
+    kangwondo: { label: '江原道', value: '강원도' },
+    chungcheongdo: { label: '忠清道', value: '충청도' },
+    chungcheongbukdo: { label: '忠清北道', value: '충청북도' },
+    chungcheongnamdo: { label: '忠清南道', value: '충청남도' },
+    kyungsangdo: { label: '慶尚道', value: '경상도' },
+    kyungsangbukdo: { label: '慶尚北道', value: '경상북도' },
+    kyungsangnamdo: { label: '慶尚南道', value: '경상남도' },
+    jeonlado: { label: '全羅道', value: '전라도' },
+    jeonlabukdo: { label: '全羅北道', value: '전라북도' },
+    jeonlanamdo: { label: '全羅南道', value: '전라남도' },
+    jejudo: { label: '済州島', value: '제주도' },
+  },
+  cn: {
+    metropolitan: { label: '大都市区', value: '수도권' },
+    seoul: { label: '首尔', value: '서울' },
+    kyeongkido: { label: '京畿道', value: '경기도' },
+    kangwon: { label: '江原', value: '강원' },
+    kangwondo: { label: '江原道', value: '강원도' },
+    chungcheongdo: { label: '忠清道', value: '충청도' },
+    chungcheongbukdo: { label: '忠清北道', value: '충청북도' },
+    chungcheongnamdo: { label: '忠清南道', value: '충청남도' },
+    kyungsangdo: { label: '庆尚道', value: '경상도' },
+    kyungsangbukdo: { label: '庆尚北道', value: '경상북도' },
+    kyungsangnamdo: { label: '庆尚南道', value: '경상남도' },
+    jeonlado: { label: '全罗道', value: '전라도' },
+    jeonlabukdo: { label: '全罗北道', value: '전라북도' },
+    jeonlanamdo: { label: '全罗南道', value: '전라남도' },
+    jejudo: { label: '济州岛', value: '제주도' },
+  },
+  vi: {
+    metropolitan: { label: 'Khu đô thị', value: '수도권' },
+    seoul: { label: 'Seoul', value: '서울' },
+    kyeongkido: { label: 'Gyeonggi-do', value: '경기도' },
+    kangwon: { label: 'Gangwon', value: '강원' },
+    kangwondo: { label: 'Gangwon-do', value: '강원도' },
+    chungcheongdo: { label: 'Chungcheong-do', value: '충청도' },
+    chungcheongbukdo: { label: 'Chungcheongbuk-do', value: '충청북도' },
+    chungcheongnamdo: { label: 'Chungcheongnam-do', value: '충청남도' },
+    kyungsangdo: { label: 'Gyeongsangdo', value: '경상도' },
+    kyungsangbukdo: { label: 'Gyeongsangbuk-do', value: '경상북도' },
+    kyungsangnamdo: { label: 'Gyeongsangnam-do', value: '경상남도' },
+    jeonlado: { label: 'Jeollado', value: '전라도' },
+    jeonlabukdo: { label: 'Jeollabuk-do', value: '전라북도' },
+    jeonlanamdo: { label: 'Jeollanam-do', value: '전라남도' },
+    jejudo: { label: 'Jeju Island', value: '제주도' },
+  },
+};
+
+function LocationSelector({ handleChange, locale }: { handleChange: () => void; locale: SupportedLanguage }) {
   return (
     <div
       style={{
@@ -138,45 +252,45 @@ function LocationSelector({ handleChange }: { handleChange: () => void }) {
       }}
     >
       <Select
-        defaultValue="서울"
+        defaultValue={localLanguageMenu[locale].seoul.value}
         style={{ width: 200 }}
         onChange={handleChange}
         options={[
           {
-            label: "수도권",
+            label: localLanguageMenu[locale].metropolitan.label,
             options: [
-              { label: "서울", value: "서울" },
-              { label: "경기도", value: "경기도" },
+              { label: localLanguageMenu[locale].seoul.label, value: localLanguageMenu[locale].seoul.value },
+              { label: localLanguageMenu[locale].kyeongkido.label, value: localLanguageMenu[locale].kyeongkido.value },
             ],
           },
           {
-            label: "강원",
-            options: [{ label: "강원도", value: "강원" }],
+            label: localLanguageMenu[locale].kangwon.label,
+            options: [{ label: localLanguageMenu[locale].kangwondo.label, value: localLanguageMenu[locale].kangwondo.value }],
           },
           {
-            label: "충청도",
+            label: localLanguageMenu[locale].chungcheongdo.label,
             options: [
-              { label: "충청북도", value: "충청북도" },
-              { label: "충청남도", value: "충청남도" },
+              { label: localLanguageMenu[locale].chungcheongbukdo.label, value: localLanguageMenu[locale].chungcheongbukdo.value },
+              { label: localLanguageMenu[locale].chungcheongnamdo.label, value: localLanguageMenu[locale].chungcheongnamdo.value },
             ],
           },
           {
-            label: "경상도",
+            label: localLanguageMenu[locale].kyungsangdo.label,
             options: [
-              { label: "경상북도", value: "경상북도" },
-              { label: "경상남도", value: "경상남도" },
+              { label: localLanguageMenu[locale].kyungsangbukdo.label, value: localLanguageMenu[locale].kyungsangnamdo.value },
+              { label: localLanguageMenu[locale].kyungsangnamdo.label, value: localLanguageMenu[locale].kyungsangbukdo.value },
             ],
           },
           {
-            label: "전라도",
+            label: localLanguageMenu[locale].jeonlado.label,
             options: [
-              { label: "전라북도", value: "전라북도" },
-              { label: "전라남도", value: "전라남도" },
+              { label: localLanguageMenu[locale].jeonlabukdo.label, value: localLanguageMenu[locale].jeonlabukdo.value },
+              { label: localLanguageMenu[locale].jeonlanamdo.label, value: localLanguageMenu[locale].jeonlanamdo.value },
             ],
           },
           {
-            label: "제주도",
-            options: [{ label: "제주도", value: "제주" }],
+            label: localLanguageMenu[locale].jejudo.label,
+            options: [{ label: localLanguageMenu[locale].jejudo.label, value: localLanguageMenu[locale].jejudo.value }],
           },
         ]}
       />
